ad0f3ed075d080b674d641050f6eec517c041e48
vrmelo <akandeabiodoun@gmail.com>
2026-02-20 17:09:42 +0100

    feat: Add NLTK-based tokenization and refactor noise word removal using a configurable set in text preprocessing.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,7 +1,9 @@
 import re
 import os
+import nltk
 
 class Text:
+    noise_words = {"de", "des", "les", "le", "dans", "la", "ces", "ce", "et", "une", "est", "que", "qui", "pour", "aux", "il", "l'", "d'", "\n", "cet", "aux", "ils", "vous", "nous", "en", "notre", "plus"}
     def __init__(self, file_path: str):
         self.content = ""
         try:
@@ -14,12 +16,15 @@ class Text:
     def preprocess_text(self):
         self.content = self.content.lower()
         self.content = re.sub(r"\d+", "", self.content)
-        self.content = re.sub(r"\bde\b|\bdes\b|\bles\b|\ble\b|\bdans\b|\bla\b|\bces\b|\bce\b {2,}", "", self.content)
-        self.content = re.sub(r"(\d|l'|d'|\n|[,;.\"<>:?«»=]+)|[ \t]{2,}", lambda m: "" if m.group(1) else " ", self.content)
+        self.content = re.sub(r"\b(" + "|".join(self.noise_words) + r")\b", "", self.content)
+        self.content = re.sub(r"([,;.\"<>:?«»=]+)|[ \t]{2,}", lambda m: "" if m.group(1) else " ", self.content)
         print(self.content)
 
-
+    def tokenize(self):
+        self.content = nltk.word_tokenize(self.content)
+        
 ecolo = Text("documents/student_01.txt")
 ecolo.preprocess_text()
+ecolo.tokenize()
 
     
